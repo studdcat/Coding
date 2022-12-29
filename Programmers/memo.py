c816816b0c7fedc9e8404d91c33f06ccd8a17fb8
정기석 <eorhwnd@naver.com>
2022-12-29 13:47:05 +0900

    2022.12.29

--- Programmers/memo.py
+++ Programmers/memo.py
@@ -1,3 +1,13 @@
-s = '12345'
-ret = len(s) - 1
-print(s[2])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return 'This is Home!'
+
+@app.route('/admin')
+def admin():
+   return 'This is admin!'
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
